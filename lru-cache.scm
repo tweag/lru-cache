95c1fbc2b2668d99041ba4eaa4168050f742dc4b
Christopher Harrison <Xophmeister@users.noreply.github.com>
2026-03-26 14:35:52 +0000

    Release info (#12)

--- lru-cache.scm
+++ lru-cache.scm
@@ -297,4 +297,4 @@
   (define (memoise/lru proc #!optional (max-size 64))
     (let ((cache (make-lru-cache max-size)))
       (lambda args
-        (cache 'entry args (lambda () (apply proc args)))))))
+        (lru-cache-entry cache args (lambda () (apply proc args)))))))
